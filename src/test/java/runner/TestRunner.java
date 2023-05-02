72688ae8979f22ab8981fb1ab3d1d04d39d3bc3b
VarshaSasikumar <varshask1903@gmail.com>
2023-05-02 21:48:10 +0300

    Report changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/Loginpage.feature",
-        plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
         monochrome = true,
         publish = true
 )
